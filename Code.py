fd4c1ace942a6b0680c4341049a28c459c6ead33
Abhinav-Mahapatra <abhinav.mahapatra10@gmail.com>
2018-03-16 14:45:10 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,10 +3,19 @@
 #Functionality : Adaptive rock paper scissors
 #Designed as a hobby project.
 
+#General Import
 import pandas as pd
 import numpy as np
 import time
 
+#Local Imports
+from sklearn.naive_bayes import GaussianNB
+
+#Creating classifier object
+classifier = GaussianNB()
+
+
+#Harcoded a initial list
 input_list = [1, 2, 3, 2, 1, 3, 1, 2, 1, 2]
 input_time = [3.665839910507202, 2.487027406692505, 3.7285304069519043, 
            1.2638773918151855, 2.0523548126220703, 2.064756393432617,
@@ -26,8 +35,6 @@ def smart_move(input_list, input_time, time_diff):
     y = dataset.iloc[:, [1]].values
     
     #Applying naive bayes classification
-    from sklearn.naive_bayes import GaussianNB
-    classifier = GaussianNB()
     classifier.fit(X,y)
 
     #predicting the output
